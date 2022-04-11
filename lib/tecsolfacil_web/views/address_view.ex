53e2ed3ed7f532373fa14eea26c6065df7b84c99
RoxoFoxo <gab.apfprado@gmail.com>
2022-04-11 20:58:27 -0300

    Feat: Generate JSON context

---
+++ lib/tecsolfacil_web/views/address_view.ex
@@ -0,0 +1,19 @@
+defmodule TecsolfacilWeb.AddressView do
+  use TecsolfacilWeb, :view
+  alias TecsolfacilWeb.AddressView
+
+  def render("index.json", %{addresses: addresses}) do
+    %{data: render_many(addresses, AddressView, "address.json")}
+  end
+
+  def render("show.json", %{address: address}) do
+    %{data: render_one(address, AddressView, "address.json")}
+  end
+
+  def render("address.json", %{address: address}) do
+    %{
+      id: address.id,
+      cep: address.cep
+    }
+  end
+end
