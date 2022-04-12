59d710f21eff84c3fd76aa7cbf02d00f5d6ba63f
RoxoFoxo <gab.apfprado@gmail.com>
2022-04-11 21:21:54 -0300

    Chore: Remove functions that won't be used

--- lib/tecsolfacil/infos.ex
+++ lib/tecsolfacil/infos.ex
@@ -4,9 +4,9 @@ defmodule Tecsolfacil.Infos do
   """
 
   import Ecto.Query, warn: false
-  alias Tecsolfacil.Repo
 
   alias Tecsolfacil.Infos.Address
+  alias Tecsolfacil.Repo
 
   @doc """
   Returns the list of addresses.
@@ -53,52 +53,5 @@ defmodule Tecsolfacil.Infos do
     %Address{}
     |> Address.changeset(attrs)
     |> Repo.insert()
-  end
-
-  @doc """
-  Updates a address.
-
-  ## Examples
-
-      iex> update_address(address, %{field: new_value})
-      {:ok, %Address{}}
-
-      iex> update_address(address, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_address(%Address{} = address, attrs) do
-    address
-    |> Address.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a address.
-
-  ## Examples
-
-      iex> delete_address(address)
-      {:ok, %Address{}}
-
-      iex> delete_address(address)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_address(%Address{} = address) do
-    Repo.delete(address)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking address changes.
-
-  ## Examples
-
-      iex> change_address(address)
-      %Ecto.Changeset{data: %Address{}}
-
-  """
-  def change_address(%Address{} = address, attrs \\ %{}) do
-    Address.changeset(address, attrs)
   end
 end
--- lib/tecsolfacil/infos/address.ex
+++ lib/tecsolfacil/infos/address.ex
@@ -1,4 +1,5 @@
 defmodule Tecsolfacil.Infos.Address do
+  @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
 
