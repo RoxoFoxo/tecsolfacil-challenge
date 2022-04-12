a61c772c808354c6ecde8162e2eb7856cf209902
RoxoFoxo <gab.apfprado@gmail.com>
2022-04-12 15:19:52 -0300

    Feat: Edit infos context and tests

--- priv/repo/migrations/20220411234853_create_addresses.exs
+++ priv/repo/migrations/20220411234853_create_addresses.exs
@@ -4,6 +4,15 @@ defmodule Tecsolfacil.Repo.Migrations.CreateAddresses do
   def change do
     create table(:addresses) do
       add :cep, :string
+      add :logradouro, :string
+      add :complemento, :string
+      add :bairro, :string
+      add :localidade, :string
+      add :uf, :string
+      add :ibge, :string
+      add :gia, :string
+      add :ddd, :string
+      add :siafi, :string
 
       timestamps()
     end
